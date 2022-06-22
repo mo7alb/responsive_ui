f98aa53e20b1f1de3f3c0e96d9dd173f1badc228
mo7alb <mo7hammad.alb@gmail.com>
2022-06-22 11:11:42 +0400

    final commit

--- App.js
+++ App.js
@@ -1,83 +1,101 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
-export default function App() {
-   return (
-      <SafeAreaView style={styles.container}>
-         <Board />
-      </SafeAreaView>
-   );
-}
-
-function Board() {
-   return (
-      <View style={styles.board}>
-         <View style={styles.row}>
-            <Square value={"x"} rightBorder={true} bottomBorder={true} />
-            <Square value={"x"} rightBorder={true} bottomBorder={true} />
-            <Square value={"x"} rightBorder={false} bottomBorder={true} />
-         </View>
-         <View style={styles.row}>
-            <Square value={"x"} rightBorder={true} bottomBorder={true} />
-            <Square value={"x"} rightBorder={true} bottomBorder={true} />
-            <Square value={"x"} rightBorder={false} bottomBorder={true} />
-         </View>
-         <View style={styles.row}>
-            <Square value={"x"} rightBorder={true} bottomBorder={false} />
-            <Square value={"x"} rightBorder={true} bottomBorder={false} />
-            <Square value={"x"} rightBorder={false} bottomBorder={false} />
-         </View>
-      </View>
-   );
-}
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+   },
+   board: {
+      width: "90%",
+      height: "90%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+   },
+   row: {
+      display: "flex",
+      flexDirection: "row",
+      height: "30%",
+      marginBottom: -60,
+   },
+   square: {
+      marginVertical: 0,
+      width: "30%",
+      height: "75%",
+      display: "flex",
+      justifyContent: "center",
+   },
+   squareText: {
+      textAlign: "center",
+      fontWeight: "bold",
+      fontSize: RFPercentage(8),
+   },
+   rightBorder: {
+      borderRightWidth: 3,
+   },
+   bottomBorder: {
+      borderBottomWidth: 3,
+   },
+});
 
 function Square({ value, rightBorder, bottomBorder }) {
    return (
       <View
          style={[
             styles.square,
-            rightBorder ? styles.rightBordered : "",
-            bottomBorder ? styles.bottomBordered : "",
+            rightBorder == true ? styles.rightBorder : "",
+            bottomBorder == true ? styles.bottomBorder : "",
          ]}
       >
-         <Text adjustsFontSizeToFit={true} style={styles.value}>
-            {value}
-         </Text>
+         <Text style={styles.squareText}>{value}</Text>
       </View>
    );
 }
 
-const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-   },
-   board: {
-      display: "flex",
-      justifyContent: "center",
-      alignContent: "center",
-      width: "85%",
-      height: "85%",
-   },
-   square: {
-      width: "33%",
-      paddingBottom: "8%",
-      paddingTop: "5%",
-   },
-   row: {
-      display: "flex",
-      flexDirection: "row",
-   },
-   value: {
-      fontSize: 60,
-      fontWeight: "bold",
-      textAlign: "center",
-   },
-   rightBordered: {
-      borderRightWidth: 7,
-   },
-   bottomBordered: {
-      borderBottomWidth: 7,
-   },
-});
+function Row({ rows }) {
+   return (
+      <View style={styles.row}>
+         {rows.map(row => (
+            <Square
+               key={row.id}
+               value={row.value}
+               rightBorder={row.right}
+               bottomBorder={row.bottom}
+            />
+         ))}
+      </View>
+   );
+}
+
+export default function App() {
+   return (
+      <SafeAreaView style={styles.container}>
+         <View style={styles.board}>
+            <Row
+               rows={[
+                  { id: 1, value: "O", right: true, bottom: true },
+                  { id: 2, value: "X", right: true, bottom: true },
+                  { id: 3, value: "O", right: false, bottom: true },
+               ]}
+            />
+            <Row
+               rows={[
+                  { id: 4, value: "X", right: true, bottom: true },
+                  { id: 5, value: "O", right: true, bottom: true },
+                  { id: 6, value: "X", right: false, bottom: true },
+               ]}
+            />
+            <Row
+               rows={[
+                  { id: 7, value: "O", right: true, bottom: false },
+                  { id: 8, value: "X", right: true, bottom: false },
+                  { id: 9, value: "O", right: false, bottom: false },
+               ]}
+            />
+         </View>
+      </SafeAreaView>
+   );
+}
